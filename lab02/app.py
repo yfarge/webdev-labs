ec2e2539a265f29aee478c119587f4e4c3c6981f
Yousef Farge <yousef.farge@gmail.com>
2022-01-13 15:33:06 -0600

    lab02 submission

--- lab02/app.py
+++ lab02/app.py
@@ -29,7 +29,7 @@ quotes = (
 ##############
 @app.route('/')
 def exercise1():
-    return 'Hello World!'
+    return 'Hi, ' + current_user.get_full_name() + "!"
 
 
 ##############
@@ -39,20 +39,21 @@ def exercise1():
 def exercise2():
     return render_template(
         'quote-of-the-day.html',
-        user=current_user
+        user=current_user,
+        quote=random.choice(quotes)
     )
 
 ##############
 # Exercise 3 #
 ##############
+@app.route('/restaurant-data/<city>/<search_term>')
+@app.route('/restaurant-data/<city>')
 @app.route('/restaurant-data')
-def exercise3():
-    import json
-    search_term = 'pizza'
-    city = 'Evanston, Il'
+def exercise3(city='Evanston, IL', search_term=''):
     url = 'https://www.apitutor.org/yelp/simple/v3/businesses/search?location={0}&term={1}'.format(city, search_term)
     response = requests.get(url)
     data = response.json()
+    pprint(data) # for debugging -- prints the result to the command line
     return json.dumps(data)
 
 ##############
